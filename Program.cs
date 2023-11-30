84753af9f138393dbb1bf29962767ae28d6ef419
Davidgomes6438 <101217381+DAVIDGOMESDEJESUS@users.noreply.github.com>
2023-11-30 17:29:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,354 @@
+﻿using POO1__ATIVIDADE_2;
+using System;
+
+namespace oat3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int validador = 0;
+
+            do
+            {
+                Console.WriteLine("sejam Bem-vindo(a)s");
+                Console.WriteLine("Escolha a opção:");
+                Console.WriteLine("Exercicios de 1 á 64");
+                Console.WriteLine(" Digite -1 Fechar o programa");
+
+                int escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        Class1 exercice1 = new Class1();
+                        exercice1.Exercice1();
+                        break;
+
+                    case 2:
+                        Class2 exercice2 = new Class2();
+                        exercice2.Exercice2();
+                        break;
+
+                    case 3:
+                        Class3 exercice3 = new Class3();
+                        exercice3.Exercice3();
+                        break;
+
+                    case 4:
+                        Class4 exercice4 = new Class4();
+                        exercice4.Exercice4();
+                        break;
+
+                    case 5:
+                        Class5 exercice5 = new Class5();
+                        exercice5.Exercice5();
+                        break;
+
+
+                    case 6:
+                        Class6 exercice6 = new Class6();
+                        exercice6.Exercice6();
+                        break;
+
+                    case 7:
+                        Class7 exercice7 = new Class7();
+                        exercice7.Exercice7();
+                        break;
+
+                    case 8:
+                        Class8 exercice8 = new Class8();
+                        exercice8.Exercice8();
+                        break;
+
+                    case 9:
+                        Class9 exercice9 = new Class9();
+                        exercice9.Exercice9();
+                        break;
+
+                    case 10:
+                        Class10 exercice10 = new Class10();
+                        exercice10.Exercice10();
+                        break;
+
+                    case 11:
+                        Class11 exercice11 = new Class11();
+                        exercice11.Exercice11();
+                        break;
+
+                    case 12:
+                        Class12 exercice12 = new Class12();
+                        exercice12.Exercice12();
+                        break;
+
+                    case 13:
+                        Class13 exercice13 = new Class13();
+                        exercice13.Exercice13();
+                        break;
+
+                    case 14:
+                        Class14 exercice14 = new Class14();
+                        exercice14.Exercice14();
+                        break;
+
+                    case 15:
+                        Class15 exercice15 = new Class15();
+                        exercice15.Exercice15();
+                        break;
+
+                    case 16:
+                        Class16 exercice16 = new Class16();
+                        exercice16.Exercice16();
+                        break;
+
+                    case 17:
+                        Class17 exercice17 = new Class17();
+                        exercice17.Exercice17();
+                        break;
+
+                    case 18:
+                        Class18 exercice18 = new Class18();
+                        exercice18.Exercice18();
+                        break;
+
+                    case 19:
+                        Class19 exercice19 = new Class19();
+                        exercice19.Exercice19();
+                        break;
+
+                    case 20:
+                        Class20 exercice20 = new Class20();
+                        exercice20.Exercice20();
+                        break;
+
+                    case 21:
+                        Class21 exercice21 = new Class21();
+                        exercice21.Exercice21();
+                        break;
+
+                    case 22:
+                        Class22 exercice22 = new Class22();
+                        exercice22.Exercice22();
+                        break;
+
+                    case 23:
+                        Class23 exercice23 = new Class23();
+                        exercice23.Exercice23();
+                        break;
+
+                    case 24:
+                        Class24 exercice24 = new Class24();
+                        exercice24.Exercice24();
+                        break;
+
+                    case 25:
+                        Class25 exercice25 = new Class25();
+                        exercice25.Exercice25();
+                        break;
+
+
+                    case 26:
+                        Class26 exercice26 = new Class26();
+                        exercice26.Exercice26();
+                        break;
+
+                    case 27:
+                        Class27 exercice27 = new Class27();
+                        exercice27.Exercice27();
+                        break;
+
+                    case 28:
+                        Class28 exercice28 = new Class28();
+                        exercice28.Exercice28();
+                        break;
+
+                    case 29:
+                        Class29 exercice29 = new Class29();
+                        exercice29.Exercice29();
+                        break;
+
+                    case 30:
+                        Class30 exercice30 = new Class30();
+                        exercice30.Exercice30();
+                        break;
+
+
+                    case 31:
+                        Class31 exercice31 = new Class31();
+                        exercice31.Exercice31();
+                        break;
+
+                    case 32:
+                        Class32 exercice32 = new Class32();
+                        exercice32.Exercice32();
+                        break;
+
+                    case 33:
+                        Class33 exercice33 = new Class33();
+                        exercice33.Exercice33();
+                        break;
+
+                    case 34:
+                        Class34 exercice34 = new Class34();
+                        exercice34.Exercice34();
+                        break;
+
+                    case 35:
+                        Class35 exercice35 = new Class35();
+                        exercice35.Exercice35();
+                        break;
+
+                    case 36:
+                        Class36 exercice36 = new Class36();
+                        exercice36.Exercice36();
+                        break;
+
+                    case 37:
+                        Class37 exercice37 = new Class37();
+                        exercice37.Exercice37();
+                        break;
+
+                    case 38:
+                        Class38 exercice38 = new Class38();
+                        exercice38.Exercice38();
+                        break;
+
+                    case 39:
+                        Class39 exercice39 = new Class39();
+                        exercice39.Exercice39();
+                        break;
+
+                    case 40:
+                        Class40 exercice40 = new Class40();
+                        exercice40.Exercice40();
+                        break;
+
+                    case 41:
+                        Class41 exercice41 = new Class41();
+                        exercice41.Exercice41();
+                        break;
+
+                    case 42:
+                        Class42 exercice42 = new Class42();
+                        exercice42.Exercice42();
+                        break;
+
+                    case 43:
+                        Class43 exercice43 = new Class43();
+                        exercice43.Exercice43();
+                        break;
+
+                    case 44:
+                        Class44 exercice44 = new Class44();
+                        exercice44.Exercice44();
+                        break;
+
+                    case 45:
+                        Class45 exercice45 = new Class45();
+                        exercice45.Exercice45();
+                        break;
+
+                    case 46:
+                        Class46 exercice46 = new Class46();
+                        exercice46.Exercice46();
+                        break;
+
+                    case 47:
+                        Class47 exercice47 = new Class47();
+                        exercice47.Exercice47();
+                        break;
+
+                    case 48:
+                        Class48 exercice48 = new Class48();
+                        exercice48.Exercice48();
+                        break;
+
+                    case 49:
+                        Class49 exercice49 = new Class49();
+                        exercice49.Exercice49();
+                        break;
+
+                    case 50:
+                        Class50 exercice50 = new Class50();
+                        exercice50.Exercice50();
+                        break;
+
+                    case 51:
+                        Class51 exercice51 = new Class51();
+                        exercice51.Exercice51();
+                        break;
+
+                    case 52:
+                        Class52 exercice52 = new Class52();
+                        exercice52.Exercice52();
+                        break;
+
+                    case 53:
+                        Class53 exercice53 = new Class53();
+                        exercice53.Exercice53();
+                        break;
+
+                    case 54:
+                        Class54 exercice54 = new Class54();
+                        exercice54.Exercice54();
+                        break;
+
+                    case 55:
+                        Class55 exercice55 = new Class55();
+                        exercice55.Exercice55();
+                        break;
+
+                    case 56:
+                        Class56 exercice56 = new Class56();
+                        exercice56.Exercice56();
+                        break;
+
+                    case 57:
+                        Class57 exercice57 = new Class57();
+                        exercice57.Exercice57();
+                        break;
+
+                    case 58:
+                        Class58 exercice58 = new Class58();
+                        exercice58.Exercice58();
+                        break;
+
+                    case 59:
+                        Class59 exercice59 = new Class59();
+                        exercice59.Exercice59();
+                        break;
+
+                    case 60:
+                        Class60 exercice60 = new Class60();
+                        exercice60.Exercice60();
+                        break;
+
+                    case 61:
+                        Class61 exercice61 = new Class61();
+                        exercice61.Exercice61();
+                        break;
+
+                    case 62:
+                        Class62 exercice62 = new Class62();
+                        exercice62.Exercice62();
+                        break;
+
+                    case 63:
+                        Class63 exercice63 = new Class63();
+                        exercice63.Exercice63();
+                        break;
+
+                    case -1:
+                        Console.WriteLine("O programa será fechado");
+                        Console.Read();
+                        validador = -1;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            } while (validador != -1);
+        }
+    }
+}
